eec3e6bb456996d6c2d92c60ba734adb10e11fbe
dburgos <david.burgos.risco@gmail.com>
2013-07-27 22:23:11 +0200

    Fix of <<<HEAD and branch strings

--- js/views/connection.js
+++ js/views/connection.js
@@ -11,7 +11,6 @@ window.ConnectionView = Backbone.View.extend({
      },
     
     connectDb: function (e) {
-<<<<<<< HEAD
     	 $.ajax({
 			  type: 'POST',
 			  url: "index.php/quickcim/connect",
@@ -43,37 +42,3 @@ window.ConnectionView = Backbone.View.extend({
 		});
     }
 });
-=======
-    	 
-    	 $.ajax({
-			  type: 'POST',
-			  url: "quickcim/connect",
-			  data: $("#connection").serialize(),
-			  dataType: "json",
-			  error: function(data) 
-			  	{
-					utils.toogleFade("#connection-error", 5000);
-			  	},
-			  success: function(data) 
-			  	{
-					if (data.length == 0)
-					{
-						utils.toogleFade("#no-tables", 5000);
-					}
-					else
-					{
-						$("#database-controllers").show();				
-						var tables = "";
-				     	$.each(data, function(index, value) 
-				     	{ 
-				     		tables += utils.newCheckTable(index, data[index].table);
-				     	});
-				     	$("table.table > tbody").html(tables);
-				     	$("#connection-container").removeClass("offset4");
-				    	$("#tables-container, #output-container").fadeIn("slow");
-					}
-			   }
-		});
-    }
-});
->>>>>>> branch 'master' of https://github.com/dburgos/QuickCIM.git
